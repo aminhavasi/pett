5d74d6d43d0bd3891cc568368c36cae393486bb2
amin havasi <pau.ahq@gmail.com>
2020-11-18 21:54:41 +0330

    test

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,23 +6,25 @@ const persianDate = require('persian-date');
 persianDate.toLocale('en');
 const date = new persianDate().format('YYYY/M/DD');
 router.post('/register', async (req, res) => {
-    try {
-        const user = {
-            name: 'amin',
-            family: 'havasi',
-            username: 'amin_offline',
-            email: 'amin@gmail.com',
-            adminLevel: 'customer',
-            password: '123456789',
-            date: '1375/7/24',
-            bornDate: '1375/7/24',
-        };
-        const newUser = await new User(user);
-        await newUser.save();
-        res.send('ok');
-    } catch (err) {
-        console.log(err);
-        res.status(400).send('err');
-    }
+    console.log(req.body);
+    res.status(200).send('salam alaykom');
+    // try {
+    //     const user = {
+    //         name: 'amin',
+    //         family: 'havasi',
+    //         username: 'amin_offline',
+    //         email: 'amin@gmail.com',
+    //         adminLevel: 'customer',
+    //         password: '123456789',
+    //         date: '1375/7/24',
+    //         bornDate: '1375/7/24',
+    //     };
+    //     const newUser = await new User(user);
+    //     await newUser.save();
+    //     res.send('ok');
+    // } catch (err) {
+    //     console.log(err);
+    //     res.status(400).send('err');
+    // }
 });
 module.exports = router;
